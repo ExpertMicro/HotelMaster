e399be6f482b2b5c07a7ddbb791ab2e6d2cf4072
oziel <oziel.silva222@gmail.com>
2020-09-27 19:43:00 -0300

    adicionado carousel sessao localizacao

--- js/script.js
+++ js/script.js
@@ -6,5 +6,19 @@ $(function() {
     prevArrow: $('#arrow-prev'),
     nextArrow: $('#arrow-next'),
   });
+
+  $('.slide').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: false,
+    infinite: true,
+    speed: 1000,
+    fade: true,
+    slidesToShow: 1,
+    arrows: false,
+    slide: 'div',
+    cssEase: 'linear'
+});
+
 })
 
